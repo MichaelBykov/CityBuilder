05233b9a6e9ac2e6954b891ddfaa72dfde61da98
Michael Bykov <michaelbykov@hotmail.com>
2023-04-21 19:21:48 -0700

    Clean up

--- include/CityBuilder/Geometry/RadiusPath2.h
+++ include/CityBuilder/Geometry/RadiusPath2.h
@@ -26,47 +26,6 @@ struct RadiusPath2 {
   
   
   
-  /// An intersection between two radius paths.
-  struct Intersection {
-    enum {
-      /// The other path intersects the radius path but does not cross the
-      /// center line at all.
-      /// \remarks
-      ///   The intersection point is the closest center path point.
-      brush,
-      /// The other path crosses through the radius path completely.
-      /// \remarks
-      ///   The intersection point is the point at which the two center lines
-      ///   cross.
-      crossing,
-      /// The other path intersects the radius path and crosses the center line
-      /// but does not go to the other side of the radius path completely.
-      /// \remarks
-      ///   The intersection point is the point at which the two center lines
-      ///   cross.
-      ///   If the path crosses back over the center line, there will be a
-      ///   second intersection adjacent to this one about that intersection.
-      reflection,
-      /// The other path ends at the center line of the radius path.
-      /// \remarks
-      ///   The intersection point is the end point of the other path.
-      end
-    } type;
-    
-    /// The point of intersection.
-    Real2 point;
-  };
-  
-  /// Find all the intersections between this radius path and another radius
-  /// path.
-  /// \param[in] other
-  ///   The other radius path to intersect this path with.
-  /// \returns
-  ///   A list of the found intersections or an empty list if none were found.
-  // List<Intersection> intersections(const RadiusPath2 &other);
-  
-  
-  
   /// Check if this radius path intersects another radius path in a valid way.
   /// \param[in] other
   ///   The other radius path to check for intersection with.
